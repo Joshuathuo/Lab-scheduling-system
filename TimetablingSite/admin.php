57361d26aca8d78fe885d019d57b05591a026f72
JM Wainaina <78029949+Joshuathuo@users.noreply.github.com>
2022-04-07 01:16:04 +0300

    Add files via upload

---
+++ TimetablingSite/admin.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/student.css">
+
+    <title>Login</title>
+</head>
+<body>
+    <div class="login-body">
+        <div class="login-body-content">
+            <div class="title">
+                <h1>Login Here</h1>
+            </div>
+            
+            <div class="login-main-section">
+                <div class="main-section-content">
+                    <form action="">
+                        <div class="form-main">
+    
+                            <div class="form-content">
+                                <div class="form-details">
+                                    
+                                    <div class="form-reg">
+                                        <input type="text" class="reg" placeholder="Username" required />
+                                    </div>
+    
+                                    <div class="form-pass">
+                                        <input type="password" class="pass" placeholder="password" required />
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div class="form-btn">
+                                <button type="submit">Login</button>
+                            </div>
+                            <div class="register-text">
+                                <p class="register-text">Don't have an account? <a href="register.php">Register</a></p>
+    
+    
+                            </div>
+                           
+     
+                        </div>
+                    </form>
+                </div>
+    
+            </div>
+    
+
+        </div>
+    </div>
+    
+</body>
+</html>
