cab3e7011bdcd2ef9df0f26fa4521576a2687017
JM Wainaina <78029949+Joshuathuo@users.noreply.github.com>
2022-05-07 08:48:54 +0300

    Delete TimetablingSite directory

--- TimetablingSite/admin/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername="localhost";
-$username="root";
-$password="";
-$database="computer_lab";
-
-$conn=mysqli_connect($servername,$username,$password,$database) or die("Connection failed: " . mysqli_connect_error());
- ?>
